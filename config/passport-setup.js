caa7abd6196a00343e432792082fe2382fedb1f5
Nehemiah <nehemiah@hometalk.com>
2018-11-01 18:10:20 +0200

    more files

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,6 +1,8 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
 const keys = require('./keys');
+const User = require('../models/user-model');
+
 
 //attach google strat to passport
 passport.use(
@@ -13,5 +15,12 @@ passport.use(
     //passport callback function 
     console.log('passport callback function fired');
     console.log(profile);
+
+    new User({
+        username:profile.displayName,
+        googleId:profile.id
+    }).save().then((newUser) => {
+        console.log('new user created:' + newUser);
+    })
 })
 )
